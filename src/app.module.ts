d109ce5efda2c9acb5b55f3745f6741e62654375
Tarcizio Santos <tarcizio.junior2318@outlook.com>
2024-06-21 11:27:53 -0300

    feat: create jwt controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controller/create-user.controller'
 
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
+import { AuthenticationController } from './controller/authentication.controller'
+
 @Module({
-  imports: [],
-  controllers: [CreateAccountController],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [CreateAccountController, AuthenticationController],
   providers: [PrismaService],
 })
 export class AppModule {}
